4e498afad404b8c75323a7cdfbe5be7b092d541d
xBlazeTECH <xBlazeTECH@users.noreply.github.com>
2020-06-08 12:27:39 -0400

    Using Handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@Nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+  app.setViewEngine('hbs');
+  
   await app.listen(3000);
 }
 bootstrap();
